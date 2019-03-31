a04b709a7bc81a93e14e3e901d5216b56acb54c4
FloChiff <floriane.chiffoleau@hotmail.fr>
2019-03-31 18:51:54 +0200

    Ajout des fichiers app et constantes

---
+++ app/app.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+import os
+#Package importé pour pouvoir faire des opérations liées au système
+
+from .constantes import SECRET_KEY, CONFIG
+
+chemin_actuel = os.path.dirname(__file__)
+templates = os.path.join(chemin_actuel, "templates")
+statics = os.path.join(chemin_actuel, "static")
+#On définit ici les chemins pour faire fonctionner notre site, de façon à ce que le système sache où aller chercher
+# les informations nécessaires pour que le site apparaisse correctement.
+
+
+app = Flask(__name__, template_folder=templates, static_folder=static)
+
+app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///.db/plateformememoires.sqlite'
+
+db = SQLAlchemy(app)
+#Initiation de l'objet SQLAlchemy avec l'app comme variable et stockage de la base de données dans db
+
+login = LoginManager(app)
+#Gestion d'utilisateur-rice-s
+
+from .routes import accueil, inscription, connexion, deconnexion, memoire, liste_memoires, recherche, formulaire
+
+def config_app(config_name="test"):
+    """ Create the application """
+    app.config.from_object(CONFIG[config_name])
+
+    db.init_app(app)
+    login.init_app(app)
+
+    return app
---
+++ app/constantes.py
@@ -0,0 +1,21 @@
+from warnings import warn
+
+SECRET_KEY= "JE SUIS UN SECRET !"
+
+if SECRET_KEY == "JE SUIS UN SECRET !":
+    warn("Le secret par défaut n'a pas été changé, vous devriez le faire", Warning)
+
+class _TEST:
+    SECRET_KEY = SECRET_KEY
+    SQLALCHEMY_DATABASE_URI = 'sql:///test_db.sqlite'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class _PRODUCTION:
+    SECRET_KEY = SECRET_KEY
+    SQLALCHEMY_DATABASE_URI = 'sqlite: // /.db/plateformememoires.sqlite'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+CONFIG = {
+    "test": _TEST
+    "production": _PRODUCTION
+}
