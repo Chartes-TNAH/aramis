4f2a2bd5cc4d8024b29389341bf1c867907fa685
Marine <marine-julie.frey@laposte.net>
2019-04-01 19:08:14 +0200

    rectification majuscules

--- app/modeles/donnees.py
+++ app/modeles/donnees.py
@@ -4,8 +4,8 @@ from ..app import db
 # import de la base de données sqlite
 
 a_keyword = db.Table("a_keyword",
-    db.Column("keyword", db.Integer, db.ForeignKey("keyword.keyword_id"), primary_key=True),
-    db.Column("memoire", db.Integer, db.ForeignKey("memoire.memoire_id"), primary_key=True))
+    db.Column("Keyword", db.Integer, db.ForeignKey("keyword.keyword_id"), primary_key=True),
+    db.Column("Memoire", db.Integer, db.ForeignKey("memoire.memoire_id"), primary_key=True))
 # Table d'assocation n-à-n, nécessaire à la mise en relation des tables keyword et memoire afin d'assigner des
 # mots-clés aux mémoires.
 
@@ -15,12 +15,12 @@ class Memoire(db.Model):
     memoire_id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True)
     memoire_titre = db.Column(db.String)
     memoire_auteur = db.Column(db.Integer, db.ForeignKey('utilisateur_id'))
-    auteur = db.Relationship("utilisateur", back_populates="memoire")
+    auteur = db.Relationship("Utilisateur", back_populates="memoire")
     memoire_annee = db.Column(db.Integer)
     memoire_institution = db.Column(db.String)
     memoire_tuteur = db.Column(db.Integer, db.ForeignKey('utilisateur_id'))
-    tuteur = db.Relationship("utilisateur", back_populates="memoire")
-    keyword = db.Relationship("keyword", secondary=a_keyword, backref=db.backref("memoire"))
+    tuteur = db.Relationship("Utilisateur", back_populates="memoire")
+    keyword = db.Relationship("Keyword", secondary=a_keyword, backref=db.backref("memoire"))
 
 # Table recensant les différents mots-clés à attribuer aux mémoires.
 class Keyword(db.Model):
