02784358e2efd7df0338ba89143bbdf6f8ee5ba0
FloChiff <floriane.chiffoleau@hotmail.fr>
2019-06-30 11:22:11 +0200

    Modifications pour la création de mémoires

--- app/modeles/donnees.py
+++ app/modeles/donnees.py
@@ -36,7 +36,7 @@ class Memoire(db.Model):
         :param annee: Année de rédaction
         :param tuteur: Tuteur de l'auteur
         :param institution: Institution d'accueil
-        :param motscles: Mots-clefs définissant le sujet du mémoire
+        :param motclef: Mots-clefs définissant le sujet du mémoire
         """
 
         erreur = []
@@ -50,7 +50,7 @@ class Memoire(db.Model):
             erreur.append("Il faut indiquer un tuteur")
         if not institution:
             erreur.append("L'institution doit être indiquée")
-        if not motscles:
+        if not motclef:
             erreur.append("Il faut attribuer un mot-clef")
 
         # Si on a une erreur, on doit afficher ces messages.
@@ -64,7 +64,7 @@ class Memoire(db.Model):
             memoire_annee=annee,
             memoire_tuteur=tuteur,
             memoire_institution=institution,
-            memoire_motscles=motscles
+            memoire_motclef=motclef
         )
         print(memoire)
 
